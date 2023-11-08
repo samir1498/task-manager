ff053482087f51f266c21a20fa7285da6428addc
samir1498 <samirsbx2@gmail.com>
2023-11-08 17:05:27 +0100

    task type and some helper functions

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,13 +1,22 @@
 // types.ts
+import { z } from "zod";
 
-export type Task = {
-    id: number;
-    title: string;
-    description: string;
-    dueDate: string;
-    priority: string;
-    status: string;
-  };
-  
+export const TaskSchema = z
+  .object({
+    id: z.number().or(z.undefined()),
+    title: z
+      .string()
+      .min(3, { message: "Title should be at least 3 characters long." })
+      .default(""),
+    description: z
+      .string()
+      .min(5, { message: "Description should be at least 5 characters long." }),
+    dueDate: z.date(),
+    priority: z.enum(["High", "Medium", "Low"], {
+      errorMap: (_issue, _ctx) => ({ message: "You must choose an option" }),
+    }),
+    status: z.enum(["In Progress", "Completed"]).default("In Progress"),
+  })
+  .required();
 
-  
+export type TaskType = z.infer<typeof TaskSchema>;
