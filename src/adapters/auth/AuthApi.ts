4758ab8e2c407b97b903abd57e1850805667fd58
samir <bettahar.samir@outlook.com>
2023-11-20 15:17:31 +0100

    hopfully fixing register and login

--- src/adapters/auth/AuthApi.ts
+++ src/adapters/auth/AuthApi.ts
@@ -15,6 +15,7 @@ const AuthApi = {
           headers: {
             "Content-Type": "application/json",
           },
+          withCredentials: true,
         },
       )
       if (response.status === 403) {
@@ -41,6 +42,7 @@ const AuthApi = {
           headers: {
             "Content-Type": "application/json",
           },
+          withCredentials: true,
         },
       )
 
